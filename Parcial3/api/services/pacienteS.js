e570a5cb8c345e49ceb1eb6169a81dde8a601136
erikaleonq <erikaleonq@gmail.com>
2024-11-16 10:04:47 -0500

    Parcial 3.1 - Cambiar IP para el host

--- Parcial3/api/services/pacienteS.js
+++ Parcial3/api/services/pacienteS.js
@@ -1,12 +1,27 @@
-import client from '../../database.js';
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Client } = pg;
+
 
 class PatientService {
     static async getPatientById(patientId) {
         try {
+            const client =  new Client({
+                user: process.env.DB_USER,
+                host: process.env.DB_HOST,
+                database: process.env.DB_NAME,
+                password: process.env.DB_PASSWORD,
+                port: process.env.DB_PORT,
+            });
             await client.connect();
             console.log("Conectando")
             const result = await client.query('SELECT * FROM patient WHERE id = $1', [patientId]);
             console.log(result.rows)
+
+            client.end();
         
             if (result.rows.length === 0) {
                 return null;
